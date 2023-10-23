0d99037f89f0cef69b5231c66c2c9a982336a608
Manuel Oporta <manuopca7778@gmail.com>
2023-10-22 23:44:10 -0600

    Se hizo el codigo del 1 al 100

--- python/Adivina el numero.py
+++ python/Adivina el numero.py
@@ -1,1 +1,25 @@
-# Aquí deberia ir algo
+import random
+
+# Genera un número aleatorio entre 1 y 100
+numero_secreto = random.randint(1, 100)
+
+# Solicita la entrada del jugador
+intentos = 0
+adivinado = False
+while not adivinado:
+    suposicion = input("Adivina el número (entre 1 y 100): ")
+    suposicion = int(suposicion)
+    intentos += 1
+
+    # Compara la suposición del jugador con el número secreto
+    if suposicion == numero_secreto:
+        adivinado = True
+        print("¡Acertaste! El número secreto era", numero_secreto)
+    else:
+        if suposicion < numero_secreto:
+            print("Tu suposición es demasiado baja.")
+        else:
+            print("Tu suposición es demasiado alta.")
+
+# Imprime el número de intentos realizados
+print("Hiciste", intentos, "intentos.")
